be1336ab30e81cde49f61f3cbc2949096f6e5b3e
Taiful2740 <taijeslove@gmail.com>
2023-09-13 12:13:52 +0600

    first commit

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className="text-5xl text-center font-bold my-5">Knowledge Cafe</h1>
+    </div>
+  );
+};
+
+export default Header;
